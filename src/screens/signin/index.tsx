b9c953326aa832e3e19dd57940e9a742d3666dc1
Kaique Dirceu <dirceukaique@gmail.com>
2021-06-21 16:02:09 -0300

    components

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
 
+} from 'react-native'
+
+import { ButtonIcon } from '../../components/Buttonicon'
+import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles';
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Hello word, NLW together!</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`}
+          e organize suas jogatinas {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
