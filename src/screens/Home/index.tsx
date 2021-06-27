7344fe2b2f9535ea597eb2dd31e3a26a7cb3edd6
Kaique Dirceu <dirceukaique@gmail.com>
2021-06-26 22:54:40 -0300

    add AppointmentDetails

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
@@ -7,11 +8,14 @@ import { Profile } from '../../components/Profile';
 import { ListHeader } from '../../components/ListHeader';
 import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
+
 
 import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+  const navigation = useNavigation();
 
   const appointments = [
     {
@@ -44,36 +48,45 @@ export function Home() {
     categoryId === category ? setCategory('') : setCategory(categoryId);
   }
 
+  function handleAppointmentDetails() {
+    navigation.navigate('AppointmentDetails');
+  }
+
   return (
-    <View>
-      <View style={styles.header}>
-        <Profile />
-        <ButtonAdd />
-      </View>
+    <Background>
+      <View>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd />
+        </View>
 
-      <CategorySelect
-        categorySelected={category}
-        setCategory={handleCategorySelect}
-      />
-
-      <View style={styles.content}>
-        <ListHeader
-          title="Partidas agendadas"
-          subtitle="Total 6"
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
         />
 
-        <FlatList
-          data={appointments}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <Appointment data={item} />
-          )}
-          ItemSeparatorComponent={() => <ListDivider />}
-          style={styles.matches}
-          showsVerticalScrollIndicator={false}
-        />
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
 
+          <FlatList
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Appointment
+                data={item}
+                onPress={handleAppointmentDetails}
+              />
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+          />
+
+        </View>
       </View>
-    </View>
+    </Background>
   )
 }
--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -7,6 +7,8 @@ import {
 import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/Buttonicon'
+import { Background } from '../../components/Background';
+
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles';
 
@@ -18,30 +20,32 @@ export function SignIn() {
   }
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={IllustrationImg}
-        style={styles.image}
-        resizeMode="stretch"
-      />
-
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {`\n`}
-          e organize suas jogatinas {`\n`}
-          facilmente
-        </Text>
-
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
-          favoritos com seus amigos
-        </Text>
-
-        <ButtonIcon
-          title="Entrar com Discord"
-          onPress={hadleSignIn}
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={IllustrationImg}
+          style={styles.image}
+          resizeMode="stretch"
         />
+
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {`\n`}
+            e organize suas jogatinas {`\n`}
+            facilmente
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com seus amigos
+          </Text>
+
+          <ButtonIcon
+            title="Entrar com Discord"
+            onPress={hadleSignIn}
+          />
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
